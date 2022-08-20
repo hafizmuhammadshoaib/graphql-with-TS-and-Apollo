c81395662c2d4f51e91cd003c1ac24eac0c63aeb
hafizmuhammadshoaib <shoaibsilat9@gmail.com>
2022-08-20 22:43:45 +0500

    added user and auth layer

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -2,11 +2,21 @@ import { extendType, intArg, nonNull, objectType, stringArg } from "nexus";
 import { NexusGenObjects } from "../../nexus-typegen";
 
 export const Link = objectType({
-  name: "Link", // <- Name of your type
+  name: "Link",
   definition(t) {
     t.nonNull.int("id");
     t.nonNull.string("description");
     t.nonNull.string("url");
+    t.field("postedBy", {
+      // 1
+      type: "User",
+      resolve(parent, args, context) {
+        // 2
+        return context.prisma.link
+          .findUnique({ where: { id: parent.id } })
+          .postedBy();
+      },
+    });
   },
 });
 
@@ -61,26 +71,32 @@ export const LinkByIdQuery = extendType({
 });
 
 export const LinkMutation = extendType({
-  // 1
   type: "Mutation",
   definition(t) {
     t.nonNull.field("post", {
-      // 2
       type: "Link",
       args: {
-        // 3
         description: nonNull(stringArg()),
         url: nonNull(stringArg()),
       },
-
       resolve(parent, args, context) {
-        const { description, url } = args; // 4
-        return context.prisma.link.create({
+        const { description, url } = args;
+        const { userId } = context;
+
+        if (!userId) {
+          // 1
+          throw new Error("Cannot post without logging in.");
+        }
+
+        const newLink = context.prisma.link.create({
           data: {
             description,
             url,
+            postedBy: { connect: { id: userId } }, // 2
           },
         });
+
+        return newLink;
       },
     });
   },
